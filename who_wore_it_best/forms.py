6d79929c6df35fe2fb822edce8b915b99c6913d9
gilgamesh7 <felidaesrule@yahoo.com>
2022-06-08 23:01:44 +1200

    Voting dropdowns implemented. Issue with pk

--- who_wore_it_best/forms.py
+++ who_wore_it_best/forms.py
@@ -4,10 +4,23 @@ from who_wore_it_best.models import PeopleVote
 
 people = [
     ('', ''),
-    ('Dee', 'Dee Williamson'),
-    ('Tony', 'Anthony Bus'),
-    ('Roopal', 'Roopal Vishwakarma'),
-    ('Srikar', 'Srikar vangala')
+    ('Dee Williamson', 'Dee Williamson'),
+    ('Anthony Bus', 'Anthony Bus'),
+    ('Roopal Vishwakarma', 'Roopal Vishwakarma'),
+    ('Srikar Vangala', 'Srikar Vangala'),
+    ('Vishaal Prasad', 'Vishaal Prasad'),
+    ('Sudha Sachidanandam', 'Sudha Sachidanandam'),
+    ('Stephen Gupwell', 'Stephen Gupwell'),
+    ('Ramanjulu Sunkari', 'Ramanjulu Sunkari'),
+    ('Rajesh Babu', 'Rajesh Babu'),
+    ('Pawan Kumar', 'Pawan Kumar'),
+    ('Norman Close', 'Norman Close'),
+    ('Malishka Walpole Lokuge', 'Malishka Walpole Lokuge'),
+    ('Sourav Shaw', 'Sourav Shaw'),
+    ('Naresh Seth', 'Naresh Seth'),
+    ('Mark Annas', 'Mark Annas'),
+    ('Felix Lee', 'Felix Lee'),
+    ('Bernard OLeary', 'Bernard OLeary'),
 ]
 
 votes = [
@@ -18,13 +31,13 @@ votes = [
 class VoteForm(forms.ModelForm):
     class Meta:
         model = PeopleVote
-        fields = '__all__'
+        fields = ['name','vote']
 
+        widgets = {
+            'name' : forms.Select(attrs={'class': 'form-control'}, choices=people),
+            'vote' : forms.Select(attrs={'class': 'form-control'}, choices=votes),
+        }
         # widgets = {
-        #     'name' : forms.Select(attrs={'class': 'form-control'}, choices=people),
+        #     'name' : forms.TextInput(attrs={'class': 'form-control'}),
         #     'vote' : forms.Select(attrs={'class': 'form-control'}, choices=votes),
         # }
-        widgets = {
-            'name' : forms.TextInput(attrs={'class': 'form-control'}),
-            'vote' : forms.TextInput(attrs={'class': 'form-control'}),
-        }
