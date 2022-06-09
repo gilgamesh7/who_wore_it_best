244e27ccd17a12afbf2c3bd8624aa92c06b32658
gilgamesh7 <felidaesrule@yahoo.com>
2022-06-09 21:38:19 +1200

    Static dir added in settings.py

--- vote_rajesh/settings.py
+++ vote_rajesh/settings.py
@@ -16,9 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Heroku needs this
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# For serving in Production mode
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -27,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ejgc8!j0k6uyr11q*fg)))k&%q*$2t!#0%hj$bf&5_k8ml=ld2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['whoworeitbest.azurewebsites.net','127.0.0.1']
 
